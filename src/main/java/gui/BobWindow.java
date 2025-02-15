3769852ff484890c96286e93265332eff96bdd9b
Andy Chong <andychong23@gmail.com>
2025-02-16 00:55:03 +0800

    Implemented button clicking and enter keystroke events to trigger certain actions
    Implemented setDefaultWindow to handle set up of the window for Bob GUI
    Added setDefaultVBox, setDefaultScrollPane, setDefaultTextField to handle set up of
    different GUI objects for BobWindow
    
    Implemented handleEnter and handleButtonClick methods to trigger actions based on user inputs

--- src/main/java/gui/BobWindow.java
+++ src/main/java/gui/BobWindow.java
@@ -1,44 +1,90 @@
 package gui;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class BobWindow extends AnchorPane {
     private ScrollPane scrollPane;
     private VBox vBox;
+    private HBox hBox;
     private TextField textField;
     private Button button;
+    private Image bobImage = new Image(this.getClass().getResourceAsStream("/images/bob.jpg"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/gru.jpg"));
 
     public BobWindow(double minHeight, double minWidth) {
-        this.setMinHeight(minHeight);
-        this.setMinWidth(minWidth);
+        setDefaultWindow(minHeight, minWidth);
+
+        this.setOnKeyPressed((event) -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                handleEnter();
+            }
+        });
+        this.button.setOnAction((event) -> {
+            handleButtonClick();
+        });
+    }
+
+    private void setDefaultWindow(double minHeight, double minWidth) {
+        this.setPrefSize(minWidth, minHeight);
 
         scrollPane = new ScrollPane();
-
         vBox = new VBox();
 
+        scrollPane.setContent(vBox);
+
+        hBox = new HBox();
         textField = new TextField();
         button = new Button("Send");
 
-        vBox.getChildren().addAll(scrollPane, textField, button);
+        hBox.getChildren().addAll(textField, button);
+        hBox.setPrefWidth(this.getPrefWidth());
+        textField.setPrefWidth(hBox.getPrefWidth() * 0.8);
+        button.setPrefWidth(hBox.getPrefWidth() * 0.2);
 
         setDefaultScrollPane(scrollPane);
+        setDefaultTextField(textField);
+        setTopAnchor(scrollPane, 0.0);
+        setBottomAnchor(hBox, 0.0);
         setDefaultVBox(vBox);
 
-        this.getChildren().addAll(vBox);
-    }
-
-    private void setDefaultScrollPane(ScrollPane scrollPane) {
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-        scrollPane.setFitToHeight(true);
-        scrollPane.setFitToWidth(true);
+        this.getChildren().addAll(scrollPane, hBox);
     }
 
     private void setDefaultVBox(VBox vBox) {
-        vBox.setMinSize(this.getMinWidth(), this.getMinHeight());
+        vBox.setPrefWidth(scrollPane.getPrefWidth());
+    }
+
+    private void setDefaultScrollPane(ScrollPane scrollPane) {
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setPrefSize(this.getPrefWidth(), this.getPrefHeight());
+        scrollPane.vvalueProperty().bind(vBox.heightProperty());
+    }
+
+    private void setDefaultTextField(TextField textField) {
+        textField.setPromptText("Input your request here");
+    }
+
+    private void handleEnter() {
+        handleButtonClick();
+    }
+
+    private void handleButtonClick() {
+        DialogBox userDialog = DialogBox.createUserDialogBox(userImage, textField.getText());
+        DialogBox bobDialog = DialogBox.createBobDialogBox(bobImage, "this is a test");
+
+        userDialog.setAlignment(Pos.CENTER_RIGHT);
+        bobDialog.setAlignment(Pos.CENTER_LEFT);
+
+        vBox.getChildren().addAll(userDialog, bobDialog);
+        textField.clear();
     }
 }
