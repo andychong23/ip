fc246f2c7262cd02f80a7d0a6054bb6e33515c24
Andy Chong <andychong23@gmail.com>
2025-02-16 00:52:45 +0800

    Set default size for images and enable text wrapping
    Implement static methods createUserDialogBox and createBobDialogBox
    This allows different dialog box of different formats to be created
    bobDialogBox will have its contents reversed from userDialogBox

--- src/main/java/gui/DialogBox.java
+++ src/main/java/gui/DialogBox.java
@@ -1,26 +1,46 @@
 package gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
+import java.util.Collections;
+
 public class DialogBox extends HBox {
+    private final int IMAGE_WIDTH = 150;
+    private final int IMAGE_HEIGHT = 150;
+
     public DialogBox(Image img, String text) {
         ImageView imageView = new ImageView(img);
         Label label = new Label(text);
-
-        this.getChildren().addAll(imageView, label);
+        setImageViewSize(imageView);
+        setLabelSize(label);
+        this.getChildren().addAll(label, imageView);
     }
 
     private void setImageViewSize(ImageView imgView) {
-        imgView.setFitWidth(this.getWidth() * 0.8);
-        imgView.setFitHeight(this.getHeight());
+        imgView.setFitWidth(IMAGE_WIDTH);
+        imgView.setFitHeight(IMAGE_HEIGHT);
     }
 
     private void setLabelSize(Label label) {
         label.setWrapText(true);
-        label.setPrefWidth(this.getWidth() * 0.2);
-        label.setPrefHeight(this.getHeight());
+    }
+
+    public static DialogBox createUserDialogBox(Image img, String text) {
+        return new DialogBox(img, text);
+    }
+
+    public static DialogBox createBobDialogBox(Image img, String text) {
+        DialogBox dialogBox = new DialogBox(img, text);
+        ObservableList<Node> temp = FXCollections.observableArrayList(dialogBox.getChildren());
+        Collections.reverse(temp);
+        dialogBox.getChildren().setAll(temp);
+        return dialogBox;
     }
 }
