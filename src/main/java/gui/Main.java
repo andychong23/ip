41ff4766fefc732bae1dbb04fc212fccc0df8c98
Andy Chong <andychong23@gmail.com>
2025-02-16 14:53:01 +0800

    Change title of the stage

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -2,19 +2,19 @@ package gui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application{
     double minHeight = 600;
     double minWidth = 400;
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
         AnchorPane anchorPane = new BobWindow(minHeight, minWidth);
-
+        stage.setTitle("Bob");
         Scene scene = new Scene(anchorPane);
         stage.setScene(scene);
         stage.show();
