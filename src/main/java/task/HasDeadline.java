70690f827844dd9c09fe1f3af7f3bc3d999e1d25
Andy Chong <andychong23@gmail.com>
2025-03-08 12:03:09 +0800

    Remove redundant access modifiers in interfaces

--- src/main/java/task/HasDeadline.java
+++ src/main/java/task/HasDeadline.java
@@ -6,8 +6,8 @@ import java.time.format.DateTimeFormatter;
  * HasDeadline Interface
  */
 public interface HasDeadline {
-    static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd MMM yyyy");
-    static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd MMM yyyy hh:mma");
+    DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd MMM yyyy");
+    DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd MMM yyyy hh:mma");
 
-    public String getDeadLine();
+    String getDeadLine();
 }
